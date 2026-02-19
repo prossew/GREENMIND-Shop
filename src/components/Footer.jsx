b3e8df48085b972e5ff5cc47f212a877c8ffac25
prossew <snowniksa@gmail.com>
2026-02-19 18:16:07 +0300

    feat: add responsive footer with brand, socials, columns, and copyright

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,83 @@
 import React from 'react';
+import facebookIcon from '../assets/images/facebook__icon.svg';
+import instagramIcon from '../assets/images/instagram_icon.svg';
+import twitterIcon from '../assets/images/twitter__icon.svg';
 import './Footer.css';
 
 function Footer() {
-  return <footer></footer>;
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="footer__top">
+          <div className="footer__brand">
+            <p className="footer__name">GREENMIND</p>
+            <p className="footer__text">We help you find your dream plant</p>
+            <div className="footer__social">
+              <a href="#">
+                <img src={facebookIcon} alt="Facebook" />
+              </a>
+              <a href="#">
+                <img src={instagramIcon} alt="Instagram" />
+              </a>
+              <a href="#">
+                <img src={twitterIcon} alt="Twitter" />
+              </a>
+            </div>
+            <div className="footer__bottom">
+              <p className="footer__copyright">
+                2023 © All Rights Reserved | Terms of Use | GREENMIND
+              </p>
+            </div>
+          </div>
+
+          <div className="footer__column">
+            <h3 className="footer__title">Information</h3>
+            <ul className="footer__list">
+              <li className="footer__item">
+                <a href="#">About</a>
+              </li>
+              <li className="footer__item">
+                <a href="#">Product</a>
+              </li>
+              <li className="footer__item">
+                <a href="#">Blog</a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="footer__column">
+            <h3 className="footer__title">Company</h3>
+            <ul className="footer__list">
+              <li className="footer__item">
+                <a href="#">Community</a>
+              </li>
+              <li className="footer__item">
+                <a href="#">Career</a>
+              </li>
+              <li className="footer__item">
+                <a href="#">Our story</a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="footer__column">
+            <h3 className="footer__title">Contact</h3>
+            <ul className="footer__list">
+              <li className="footer__item">
+                <a href="#">Getting Started</a>
+              </li>
+              <li className="footer__item">
+                <a href="#">Pricing</a>
+              </li>
+              <li className="footer__item">
+                <a href="#">Resources</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,35 +8,39 @@ function Header() {
   return (
     <header className="header">
       <div className="container">
-        <a href="#">
-          <h1 className="header__title">GREENMIND</h1>
-        </a>
-        <nav>
-          <ul className="header__list">
-            <li>
-              <a href="#">Home</a>
-            </li>
-            <li>
-              <a className="" href="#">
-                Products
-              </a>
-            </li>
-            <li>
-              <a href="#">Contacts</a>
-            </li>
-          </ul>
-        </nav>
-        <div className="header__icons">
-          <a href="#">
-            <img src={cartIcon} alt="Cart" />
-          </a>
-          <a href="#">
-            <img src={personIcon} alt="Person" />
-          </a>
-          <div className="header__border"></div>
-          <a href="#">
-            <img src={navIcon} alt="navigation" />
-          </a>
+        <div className="header__block">
+          <h1 className="header__title">
+            <a href="#">GREENMIND </a>
+          </h1>
+
+          <nav>
+            <ul className="header__list">
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a className="" href="#">
+                  Products
+                </a>
+              </li>
+              <li>
+                <a href="#">Contacts</a>
+              </li>
+            </ul>
+          </nav>
+
+          <div className="header__icons">
+            <a href="#">
+              <img src={cartIcon} alt="Cart" />
+            </a>
+            <a href="#">
+              <img src={personIcon} alt="Person" />
+            </a>
+            <div className="header__border"></div>
+            <a href="#">
+              <img src={navIcon} alt="navigation" />
+            </a>
+          </div>
         </div>
       </div>
     </header>
